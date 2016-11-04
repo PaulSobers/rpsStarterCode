54f12b545e0183d9b6f015c2fb0415b9aa798aeb
paulsobers <psobe3473@withs.org>
2016-11-04 17:27:39 +0000

    completed work

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,15 +1,19 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
 var generateComputerChoice = function () {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
-
+    var Game = ["rock" , "paper" , "scissors"];
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
     // number between 0 and 2. Be sure to save it to a variable.
-
+    var num = Math.floor(Math.random() * 3);
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
-
+    console.log(num);
+    var choice = Game[num];
     // Task 1, Step 4: return this new value
-};
+    console.log(choice);
+    return choice;
+    
+}; 
 
 // Task 2: Create a function that compares the userChoice and the computerChoice
 // to decide who won.
@@ -18,12 +22,27 @@ var pickWinner = function (userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-
+    var data = ("");
+    if (userChoice === computerChoice){
+        data = "Draw"
+    }
+    if (userChoice === "rock" && computerChoice === "paper"){
+        data = "computer win"
+    }else if(computerChoice === "scissors"){
+        data = "user win"
+    }
+    if (userChoice === "scissors" && computerChoice === "paper"){
+        data = "user win" 
+    }
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
-
+    console.log(data);
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
     // Task 5: Show the winner in HTML after the word "Winner:"
+    $('#computer-choice').html(computerChoice);
+    $('#winner').html(data)
+    
+    
 };
 
 /* DOCUMENT READY: Everything inside this function will happen after
